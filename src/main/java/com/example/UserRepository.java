d1795da395621a383b91a8c54feea576f8465fd5
01GlennAM <shunretrofied@gmail.com>
2026-05-15 21:28:35 -0500

    primer commit

---
+++ src/main/java/com/example/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.List;
+
+public class UserRepository {
+
+    public void save(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    public List<User> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+}
